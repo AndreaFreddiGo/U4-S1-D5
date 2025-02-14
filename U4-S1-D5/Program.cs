b612da97e88130e85fd55a58746722cbb20b1862
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-02-14 15:22:05 +0100

    Adds new corrections

--- U4-S1-D5/Program.cs
+++ U4-S1-D5/Program.cs
@@ -10,13 +10,15 @@ namespace U4_S1_D5
 
         }
         static void CalcolatoreImposte() {
-        Console.WriteLine("==================================================\r\n               CALCOLATORE IMPOSTE\r\n==================================================");
+        Console.WriteLine("==================================================" +
+            "\r\n               CALCOLATORE IMPOSTE" +
+            "\r\n==================================================");
             Console.Write("\r\nInserisci il nome del contribuente: ");
-            string Nome = Console.ReadLine();
+            string Nome = Console.ReadLine().ToUpper();
             Console.Write("\r\nInserisci il cognome del contribuente: ");
-            string Cognome = Console.ReadLine();
+            string Cognome = Console.ReadLine().ToUpper();
             Console.Write("\r\nInserisci la data di nascita del contribuente (gg/mm/aa): ");
-            string inputData = Console.ReadLine();
+            string inputData = Console.ReadLine().ToUpper();
             DateTime DataNascita;
             if (!DateTime.TryParseExact(inputData, "dd/MM/yy", null, System.Globalization.DateTimeStyles.None, out DataNascita))
             {
@@ -26,21 +28,28 @@ namespace U4_S1_D5
             Console.Write("\r\nInserisci il codice fiscale del contribuente: ");
             string CodiceFiscale = Console.ReadLine().ToUpper();
             Console.Write("\r\nInserisci il sesso del contribuente (M o F): ");
-            string Sesso = Console.ReadLine();
-            if (Sesso != "M" && Sesso != "F")
+            string Sesso = Console.ReadLine().ToUpper();
+            if (Sesso != "M" && Sesso != "F" && Sesso != "m" && Sesso != "f")
             {
                 Console.WriteLine("\r\nSesso non valido!");
 
                 return;
             }
             Console.Write("\r\nInserisci il comune di residenza del contribuente: ");
-            string ComuneResidenza = Console.ReadLine();
+            string ComuneResidenza = Console.ReadLine().ToUpper();
             Console.Write("\r\nInserisci il reddito annuale del contribuente: ");
             int RedditoAnnuale = int.Parse(Console.ReadLine());
 
             Contribuente contribuente = new Contribuente(Nome, Cognome, DataNascita, CodiceFiscale, Sesso, ComuneResidenza, RedditoAnnuale);
 
-            Console.WriteLine($"\r\n==================================================\r\n         CALCOLO DELL'IMPOSTA DA VERSARE:\r\n\r\nContribuente: {Nome} {Cognome},\r\n\r\nnato il {DataNascita.ToString("dd/MM/yyyy")} ({Sesso}),\r\n\r\nresidente in {ComuneResidenza},\r\n\r\ncodice fiscale: {CodiceFiscale}\r\n\r\nReddito dichiarato: {RedditoAnnuale},00\r\n\r\nIMPOSTA DA VERSARE: € {contribuente.CalcoloImposta(RedditoAnnuale)}");
+            Console.WriteLine($"\r\n==================================================" +
+                $"\r\n\r\n         CALCOLO DELL'IMPOSTA DA VERSARE:" +
+                $"\r\n\r\n==================================================" +
+                $"\r\n\r\nContribuente: {Nome} {Cognome}," +
+                $"\r\n\r\nnato il {DataNascita.ToString("dd/MM/yyyy")} ({Sesso})," +
+                $"\r\n\r\nresidente in {ComuneResidenza},\r\n\r\ncodice fiscale: {CodiceFiscale}" +
+                $"\r\n\r\nReddito dichiarato: {RedditoAnnuale},00" +
+                $"\r\n\r\nIMPOSTA DA VERSARE: € {contribuente.CalcoloImposta(RedditoAnnuale)}");
 
         }
         
