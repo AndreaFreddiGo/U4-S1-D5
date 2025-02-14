1ef8ce3a0bb9aebf21d89e7ab66fce206d577e2f
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-02-14 12:21:50 +0100

    Aggiungere i file di progetto.

---
+++ U4-S1-D5/Models/Contribuente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_S1_D5.Models
+{
+    public class Contribuente
+    {
+        private string Nome { get; set; }
+        private string Cognome { get; set; }
+        private DateTime DataNascita { get; set; }
+        private string CodiceFiscale { get; set; }
+        private string Sesso { get; set; }
+        private string ComuneResidenza { get; set; }
+        private int RedditoAnnuale { get; set; }
+
+    }
+}
---
+++ U4-S1-D5/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace U4_S1_D5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
