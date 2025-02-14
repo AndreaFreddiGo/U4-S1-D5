2a1b04d91574a448af96dfe23fde5cc0c339715b
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-02-14 12:44:22 +0100

    Creates project and class

--- U4-S1-D5/Models/Contribuente.cs
+++ U4-S1-D5/Models/Contribuente.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +17,32 @@ namespace U4_S1_D5.Models
         private string Sesso { get; set; }
         private string ComuneResidenza { get; set; }
         private int RedditoAnnuale { get; set; }
+        private double Imposta { get; set; }
 
+
+        public double CalcoloImposta(int RedditoAnnuale)
+        {
+
+            if (RedditoAnnuale <= 15000)
+            {
+                return Imposta = RedditoAnnuale * 0.23;
+            }
+            else if (RedditoAnnuale <= 28000)
+            {
+                return Imposta = RedditoAnnuale * 0.27 + 3450;
+            }
+            else if (RedditoAnnuale <= 55000)
+            {
+                return Imposta = RedditoAnnuale * 0.38 + 6960;
+            }
+            else if (RedditoAnnuale <= 75000)
+            {
+                return Imposta = RedditoAnnuale * 0.41 + 17220;
+            }
+            else
+            {
+                return Imposta = RedditoAnnuale * 0.43 + 25420;
+            }
+        }
     }
 }
